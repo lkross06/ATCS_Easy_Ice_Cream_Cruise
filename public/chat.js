f672cf4c4ffc8b2ed7a6f086c39cf80870effc6c
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-11-20 10:40:46 -0800

    removed silly css for enter key

--- public/chat.js
+++ public/chat.js
@@ -23,11 +23,6 @@ function new_chat_message(message, chatlog){ //creates and returns a new chat el
 }
 
 document.getElementById("chat-send").addEventListener("click", chat_send)
-document.addEventListener("keypress", function(event) { //on enter key press
-    if (event.key == "Enter") {
-        chat_send()
-    }
-    });
 
 function chat_send(){
     let chatlog = document.getElementById("chat-log")
