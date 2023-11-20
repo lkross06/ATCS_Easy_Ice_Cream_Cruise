4d22f48d4abe3fde13689bc7ec94b19540941679
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-11-19 16:42:39 -0800

    made game selection buttons green. implemented track modals for singleplayer

--- public/modal.js
+++ public/modal.js
@@ -1,13 +1,11 @@
-window.onload = function(e){
-    document.getElementById("modalbutton1").addEventListener("click", function(e){
-        let modal = document.getElementById("modal1")
-        modal.style.display = "block";
-    })
+document.getElementById("modalbutton1").addEventListener("click", function(e){
+    let modal = document.getElementById("modal1")
+    modal.style.display = "block";
+})
 
-    window.addEventListener("click", function(e){
-        let modal = document.getElementById("modal1")
-        if (e.target == modal) {
-          modal.style.display = "none";
-        }
-    })
-}
+window.addEventListener("click", function(e){
+    let modal = document.getElementById("modal")
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+})
