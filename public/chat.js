63df101c25e05984c591e15fc71c6e67e83c665e
Kirkor <kaiserhenrichccxvii@gmail.com>
2023-11-21 10:33:43 -0800

    login, sign up, and chat are now functioning on server side.

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,10 @@
-function new_chat_message(message, chatlog){ //creates and returns a new chat element with proper attributes
-    let username = "ecvil" //TODO: when merging make this refer to some session data
+// the local user's username
+let username = sessionStorage.getItem("username") 
 
+// html element 'chat-log'
+let chatlog = document.getElementById("chat-log")
+
+function new_chat_message(message, chatlog, username){ //creates and returns a new chat element with proper attributes
     if (message == ""){
         return
     }
@@ -23,13 +27,21 @@ function new_chat_message(message, chatlog){ //creates and returns a new chat el
 }
 
 document.getElementById("chat-send").addEventListener("click", chat_send)
+// TODO: hardcoded url really sucks
+let ws = new WebSocket("ws://localhost:8008")
 
 function chat_send(){
-    let chatlog = document.getElementById("chat-log")
 
     //add new element
-    let input = document.getElementById("chat-new")
-    new_chat_message(String(input.value), chatlog)
+    let input = document.getElementById("chat-new")    
+    // websocket stuff - send the message to server
+    let packet = {
+        "method": "chat",
+        "username": username,
+        "message": String(input.value)
+    }
+
+    ws.send(JSON.stringify(packet))
 
     //remove content from input
     input.value = ""
@@ -37,6 +49,16 @@ function chat_send(){
     //if there's more than 8 messages, delete the earliest one
     if (chatlog.children.length > 8){
         chatlog.removeChild(chatlog.children.item(0))
-        
+    }
+}
+
+// websocket stuff for when the client recieves a message from the server
+ws.onmessage = message => {
+    let res = JSON.parse(message.data)
+
+    if (res.method = "chat") {
+        let usr = res.username
+        let msg = res.message
+        new_chat_message(msg, chatlog, usr)
     }
 }
--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-
-// Configure the body-parser middleware
-app.use(bodyParser.json());
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// setting up the WebSocket!
+const WebSocket = require('ws')
+const socket = new WebSocket.Server({ port: 8008 })
+
+// object containing all the users
+let users = {}
+// object containing all the running games 
+let games = {}
 
 // Serve the static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,25 +22,71 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/login.html'));
 });
-
+app.get("/game", (req, res) => {
+    res.sendFile(__dirname+"/public/game.html")
+})
 // Create a route to handle the form submission
 app.post('/submitlogin', (req, res) => {
-    // Retrieve the form data from the request body
-    const { username, password } = req.body;
-
-    // Perform any necessary validation or processing on the form data
-
-    // Respond to the client with a JSON response
-    res.json({
-        status: 'success',
-        data: {
-            username: username,
-            password: password
-        }
-    });
+    let username = req.body.username
+    let pswd = req.body.password
+    if (username in users && pswd === users[username].password) {
+        console.log( "fah zhaou logged in", users)
+        res.redirect("/game")
+    } else {
+        console.log("cocksucer lied to me")
+        res.redirect("/") 
+    }
 });
+
+app.post("/submitsignup", (req, res) => {
+    let username = req.body.username
+    let pswd = req.body.password
+    if (username in users) {
+        console.log( "cant do that chiefetn", users, username)
+        res.redirect("/")
+    } else {
+        users[username] = {"password": pswd, "friends": []}
+        console.log("cocksucer didnt lye to me signup", users)
+        res.redirect("/game") 
+    }
+})
 
 // Start the Express server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+socket.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        // message should be a JSON that has the request type, eg new game, login, join, etc
+        // json shall look as thus:
+        /**
+         * {
+         *  "method": [method - joinGame, createGame, login, chat, whisper, drive etc?]
+         *  "id": [user id]
+         * }
+         */
+        let msg = JSON.parse(message)
+        console.log(msg)
+        if (msg.method === "chat") {
+            let packet = {
+                "method": "chat",
+                "username": msg.username,
+                "message": msg.message
+            }
+            socket.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(packet))
+                }
+            })
+        }
+
+    })
+})
+
+// unique ID generator, for createing game codes. modified from https://stackoverflow.com/posts/44996682/revisions
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+} 
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substring(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
