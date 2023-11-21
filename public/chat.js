d5daddada3cec00b494c67c67c027bc3065c6ac2
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-11-20 20:30:33 -0800

    made chat resizable using js. relocated to bottom-left corner

--- public/chat.js
+++ public/chat.js
@@ -40,3 +40,64 @@ function chat_send(){
         
     }
 }
+
+//resizing chat
+let container = document.getElementById("chat-container")
+let margin = 10 //amount of pixels away from border that mouse can click and resize
+let chat_top = window.innerHeight - container.clientHeight
+let chat_right = container.clientWidth
+
+let activex = false
+let activey = false
+
+let start_mx = 0
+let start_my = 0
+
+let start_w = container.clientWidth
+let start_h = container.clientHeight
+
+window.addEventListener("mousedown", function(e) {
+    //update these 4 because they change every time the mouse is moved / chat is resized
+    start_mx = e.clientX
+    start_my = e.clientY
+
+    chat_top = window.innerHeight - container.clientHeight
+    chat_right = container.clientWidth
+
+    if (!(start_mx < chat_right - margin || start_mx > chat_right + margin)){ //mouse x is close to border of chat
+        activex = true
+        start_w = container.clientWidth
+    } else {
+        activex = false
+    }
+
+    if (!(start_my < chat_top - margin || start_my > chat_top + margin)){ //mouse y is close to border of chat
+        activey = true
+        start_h = container.clientHeight
+    } else {
+        activey = false
+    }
+
+    console.log(activex, activey)
+})
+
+window.addEventListener("mouseup", function(e){
+    activex = false
+    activey = false
+})
+
+window.addEventListener("mousemove", function(e) {
+    let new_mx = e.clientX
+    let new_my = e.clientY
+
+    let container = document.getElementById("chat-container")
+
+    if (activex) {
+        let distx = new_mx - start_mx //normal because left of page is zerio
+        container.style.width = String(start_w + distx) + "px"
+    }
+    if (activey) {
+        let disty = start_my - new_my // swapped because top of page is zero
+        container.style.height = String(start_h + disty) + "px"
+    }
+})
